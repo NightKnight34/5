372ba808d60d2e07e795126726259fcda5965f77
Ilya Burov <isip_i.i.burov@mpt.ru>
2023-02-09 01:40:01 +0300

    Много тортов не бывает

---
+++ Практическая 5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Тортики. АД
+{
+    internal class Tortik
+    {
+        public string form;
+        public string size;
+        public string taste;
+        public string glaze;
+        public string decor;
+        public string all_options;
+        public int FormPrice;
+        public int SizePrice;
+        public int TastePrice;
+        public int GlazePrice;
+        public int DecorPrice;
+        public int AllPrice;
+
+        public Tortik(int formPrice, int sizePrice, int tastePrice, int glazePrice, int decorPrice, int allPrice, string AllOptions)
+        {
+            FormPrice = formPrice;
+            SizePrice = sizePrice;
+            TastePrice = tastePrice;
+            GlazePrice = glazePrice;
+            DecorPrice = decorPrice;
+            AllPrice = allPrice;
+            all_options = AllOptions;
+        }
+    }
+}
